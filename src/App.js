d1760eaecbd4bcdac630d7861e8c2eb0688759c9
Justin <justinkmpeter@gmail.com>
2022-11-13 13:58:39 -0600

    Getting spotify user-auth set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // variables for authentication
+    const CLIENT_ID = "b136039c2bad4a978b33db20e143a30a"
+    const REDIRECT_URI = "http://localhost:3000"
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+    const RESPONSE_TYPE = "token"
+
+    // state variables
+    const [token, setToken] = useState("")
+
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+
+        // if no prev stored token, then check for hash
+        if (!token && hash) {
+            token = hash.substring(1).split("&")
+                .find(elem => elem.startsWith("access_token")).split("=")[1]
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+        }
+
+        // if there is a token, update
+        setToken(token)
+    })
+
+    const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1> Spotify React</h1>
+                { !token ?
+                        <a href={`${AUTH_ENDPOINT}
+                               ?client_id=${CLIENT_ID}
+                               &redirect_uri=${REDIRECT_URI}
+                               &response_type=${RESPONSE_TYPE}`}>
+                            Login to Spotify
+                        </a>
+                : <button onClick={logout}> Logout </button>}
+            </header>
+        </div>
+    );
 }
 
 export default App;
