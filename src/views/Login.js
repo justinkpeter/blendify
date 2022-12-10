8aa7263eeec479b3545d1c952e79d67568f889d8
Justin <justinkmpeter@gmail.com>
2022-12-09 19:32:23 -0600

    Cleaning up the Login page to a working, stable state

--- src/views/Login.js
+++ src/views/Login.js
@@ -37,9 +37,28 @@ function Login(props){
         window.location.href = loginUrl
     }
     return (
-            <div className="login__wrapper" ref={vantaRef}>
-                <h1> Blendify </h1>
-                <button onClick={goToSpotifyAuthPage}> Log in </button>
+            <div className={"h-screen w-screen "} ref={vantaRef}>
+                <div className={"relative flex"}>
+                  <div className={"absolute top-80 left-40 w-1/3 text-white"}>
+                      <div className={"text-8xl font-bold"}>
+                          <span> Discover how you </span>
+                          <span className={"text-green-500"}> listen.</span>
+                      </div>
+                      <div className={"pt-12 text-4xl text-white/80"}>
+                          <span> Explore your music taste and create awesome playlists.</span>
+                      </div>
+                      <div className={"flex mt-16"}>
+                        <button
+                            className={"p-6 w-64 rounded-full bg-green-500 font-bold transition hover:bg-green-800"}
+                            onClick={goToSpotifyAuthPage}
+                        >
+                            Continue with Spotify </button>
+                      </div>
+                  </div>
+                </div>
+                {/*<h1 className={"text-6xl"}> Discover how  </h1>*/}
+                {/*<h1 className={"text-9xl"}> you listen </h1>*/}
+                {/*<button c> Log in </button>*/}
             </div>
     )
 }
