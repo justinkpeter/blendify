89a100ea71618e57545c3210be36943412254cef
Justin <justinkmpeter@gmail.com>
2022-12-16 09:38:06 -0600

    making dashboard sections a bit more dynamic in nature

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,42 +1,56 @@
-import Navbar from './Navbar'
-import Sidebar from './Sidebar'
-import Player from './Player'
-import Content from './Content'
+import React, { useEffect, useState } from 'react'
+import Navbar from '../views/Navbar'
+import './Dashboard.css'
+import { useDataLayerValue } from "../utils/DataLayer";
 
-import {useDataLayerValue} from "../utils/DataLayer";
 
-import {useState} from 'react'
 
-export default function Dashboard(props){
+export default function Dashboard(){
+    const [ {token}, dispatch ] = useDataLayerValue();
 
-    const [{ user }] = useDataLayerValue();
-    
-    const [data, setData] = useState(false);
-    const sendData = (data) => {
-        setData(!data);
+    function getSectionPercentage(){
+        return ((window.scrollX / (document.body.scrollWidth - window.innerWidth)) * 100)
     }
-    //Sidebar  - navigation
-    //Navbar
-    //Content
-    //Footer
-
-
-    //Navbar
-    // Grid
-        //content
-
-    return (
-        <div className={"h-full flex bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-slate-800 via-fuchsia-900 to-cyan-900 "}>
-            <Navbar />
-            <div className={"grid grid-cols-5 grid-flow-row gap-4 h-screen w-full mt-24 overflow-y-scroll ;"}>
-                <Sidebar/>
-                <Content/>
-                <div className={"bg-green-300"}> Somthing on the side </div>
-                {/*<div className={"bg-green-300/5"}>  3 </div>*/}
-                {/*<div className={"bg-red-300/5"}>  4 </div>*/}
-                {/*<div className={"bg-teal-300/5"}>  5 </div>*/}
+    const Section = (props) => {
+        return(
+            <div>
+                <div className={"flex grid grid-cols-26 grid-rows-10 h-screen w-section"} id={props.name}>
+                    <div className={`col-span-full row-span-full ${props.color}`}> {props.name}</div>
+                </div>
             </div>
-            {/*<Player/>*/}
-        </div>
+        )
+    }
+
+    const [sections, setSections] = useState([
+            {name: "Favorite Artists", color: "bg-green-500"},
+            {name: "Favorite Tracks", color: "bg-blue-500"},
+            {name: "Personality Profile", color: "bg-yellow-500"},
+            {name: "Audio Analysis", color: "bg-red-500"}
+        ])
+
+    const [currentCard, setCurrentCard] = useState(sections[0].name);
+
+    useEffect(() => {
+        document.addEventListener("scroll", (e) => {
+            const currentSection = sections[Math.floor(getSectionPercentage() / 25)].name
+            setCurrentCard(currentSection)
+        });
+        document.removeEventListener("scroll", (e) => {});
+    }, [currentCard, sections]);
+
+    // useEffect(() => {console.log("the current card is: ", currentCard)}, [currentCard])
+
+    return(
+            <div>
+                <Navbar sections={sections} currentSection={currentCard}/>
+                <div className={"flex overflow-x-scroll w-fit"}>
+                    {sections.map((section) => {
+                        return(
+                            <Section key={section.name} name={section.name} color={section.color} id={section.name}/>
+                        )
+                    })}
+                </div>
+            </div>
+
     )
 }
